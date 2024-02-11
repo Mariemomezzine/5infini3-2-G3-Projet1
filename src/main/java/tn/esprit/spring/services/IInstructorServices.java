f06e6877af29f84a36d3278984d2480b86314cbf
dhiabensaada <dhiaeddine.bensaada@esprit.tn>
2024-02-12 00:02:55 +0100

    testertravail

--- src/main/java/tn/esprit/spring/services/IInstructorServices.java
+++ src/main/java/tn/esprit/spring/services/IInstructorServices.java
@@ -1,8 +1,6 @@
 package tn.esprit.spring.services;
 
-import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.Instructor;
-import tn.esprit.spring.entities.Support;
 
 import java.util.List;
 
